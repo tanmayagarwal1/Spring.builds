cb475898229e8566234762cc4a56bc4c1da554b4
tanmayagarwal1 <tanmaytemp00010@gmail.com>
2021-08-17 16:21:59 +0530

    Constructor injection using xml annotations

---
+++ Constructor Injection/src/main/java/com/uhg/constructor/tanmay/Brand.java
@@ -0,0 +1,19 @@
+package com.uhg.constructor.tanmay;
+
+public class Brand {
+	private String s;
+
+	public Brand(String s) {
+		super();
+		this.s = s;
+	}
+
+	@Override
+	public String toString() {
+		return "Brand [s=" + s + "]";
+	}
+	
+	
+	
+
+}
---
+++ Constructor Injection/src/test/java/com/uhg/constructor/tanmay/ConstructorInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhg.constructor.tanmay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructorInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
