6c80c0b350adaab3bb0fb3a6d5d26861eaf642fd
tanmayagarwal1 <tanmaytemp00010@gmail.com>
2021-08-17 15:54:22 +0530

    Property injection using Xml annotations

---
+++ Attribute Annotations/src/main/java/com/uhg/attribute/tanmay/Brand.java
@@ -0,0 +1,19 @@
+package com.uhg.attribute.tanmay;
+
+public class Brand {
+	private String s;
+
+	public String getS() {
+		return s;
+	}
+
+	public void setS(String s) {
+		this.s = s;
+	}
+	
+	public String toString() {
+		return "This is an object of brand - " + s;
+		
+	}
+
+}
