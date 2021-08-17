9b530a1655608e93cdcc360f2452f2c44074034a
tanmayagarwal1 <tanmaytemp00010@gmail.com>
2021-08-17 15:21:32 +0530

    Dependency injection using annotations based componenets

--- Component Annotations/src/main/java/com/uhg/component/tanmay/TanmayApplication.java
+++ Component Annotations/src/main/java/com/uhg/component/tanmay/TanmayApplication.java
@@ -11,7 +11,11 @@ public class TanmayApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
 		Vehicle my_vehicle = (Vehicle)context.getBean("jet");
+		Vehicle my_car = (Vehicle)context.getBean("car");
+		Vehicle my_bike = (Vehicle)context.getBean("bike");
 		my_vehicle.run();
+		my_car.run();
+		my_bike.run();
 		
 		((ClassPathXmlApplicationContext)context).close();
 	}
